090f4201ca849ce58874713e62afbdd609d645a4
Roman Sokolov <gnonasis@gmail.com>
2024-12-07 01:21:17 +0200

    Solution

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from car.models import Car
 from car.serializers import CarSerializer
 
 
-
 def serialize_car_object(car: Car) -> bytes:
     serializer = CarSerializer(car)
     return JSONRenderer().render(serializer.data)
